e80da1ef505599acfc7994ece3cc75226b290543
umyonghyeon <uyh1023@naver.com>
2020-08-02 16:40:23 +0900

    Test logic create
    -- add Stream basic login

--- src/main/java/com/test1/Test1.java
+++ src/main/java/com/test1/Test1.java
@@ -1,7 +1,22 @@
 package com.test1;
 
+import static java.util.stream.Collectors.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
 public class Test1 {
-	static final public void main(String[] args) {
-		System.out.println("hello world!");
+
+	final static public void main(String[] args) {
+		List<String> sampleList = new ArrayList<String>();
+		sampleList.add("test1");
+		sampleList.add("test2");
+
+		String targetString = sampleList.stream()
+			.filter(sample -> StringUtils.equals(sample, "test2")).collect(joining());
+
+		System.out.println(targetString);
 	}
 }
